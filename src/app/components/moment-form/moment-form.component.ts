a3c10794b667e65e94f367ef9eb789d7fe021436
Marcus Vinicius <marcusmota08@gmail.com>
2023-10-26 15:40:52 -0300

    validacao de formularios

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,4 +8,32 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MomentFormComponent {
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log('submit');
+  }
 }
